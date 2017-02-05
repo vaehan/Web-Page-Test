cfb31dbb3718380942f336dbfb5f0ae627767844
vaehan <vaehan@gmail.com>
2017-02-04 23:55:21 -0800

    Update first.js

--- first.js
+++ first.js
@@ -2,3 +2,12 @@ function myFunction()
 {
 window.document.write("Testing only - this text is coming from a separate file and a separate style sheet should also control the size of this text.");
 }
+
+function toDo()
+{
+  window.document.write("Here are a few things I need to do to begin a test program (horse racing):");
+  window.document.write("I need a random generator,");
+  window.document.write("I also need to get an input from user.");
+  window.document.write("First iteration, random 1-6, get input from user (with error checking), compare and output if it's a winner.");
+  window.document.write("Second iteration, add odds tables and wagers.");
+}
