04fee5fd32f1d0c8e2bb1480dcd09ce9d485eb77
vaehan <vaehan@gmail.com>
2017-02-04 23:57:09 -0800

    Update first.js

--- first.js
+++ first.js
@@ -5,9 +5,9 @@ window.document.write("Testing only - this text is coming from a separate file a
 
 function toDo()
 {
-  window.document.write("Here are a few things I need to do to begin a test program (horse racing):");
-  window.document.write("I need a random generator,");
-  window.document.write("I also need to get an input from user.");
-  window.document.write("First iteration, random 1-6, get input from user (with error checking), compare and output if it's a winner.");
-  window.document.write("Second iteration, add odds tables and wagers.");
+  window.document.write("Here are a few things I need to do to begin a test program (horse racing):<br>");
+  window.document.write("I need a random generator,<br>");
+  window.document.write("I also need to get an input from user.<br>");
+  window.document.write("First iteration, random 1-6, get input from user (with error checking), compare and output if it's a winner.<br>");
+  window.document.write("Second iteration, add odds tables and wagers.<br>");
 }
