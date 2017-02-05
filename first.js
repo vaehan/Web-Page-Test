1c89092cabe43d01b61ba058a2d5c4d7ca6f71be
vaehan <vaehan@gmail.com>
2017-02-04 23:50:57 -0800

    Update first.js

--- first.js
+++ first.js
@@ -1,4 +1,4 @@
 function myFunction()
 {
-document.write("Testing only - this text is coming from a separate file and a separate style sheet should also control the size of this text.");
+window.document.write("Testing only - this text is coming from a separate file and a separate style sheet should also control the size of this text.");
 }
