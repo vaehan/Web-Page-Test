69c5cdc479032d303e07ed611013b793da74e398
vaehan <vaehan@gmail.com>
2017-02-07 06:58:25 -0800

    Removed script from HTML
    Placed all js code back into the js file - allowed the results to be
    pulled multiple times.

--- first.js
+++ first.js
@@ -13,21 +13,8 @@ function WinningHorse()
   return GetRandomHorse(1, 6);
 }
 
-function doSomething(Winner)
-{
-	var x = document.getElementById("UserGuess").value;
-	document.getElementById("demo").innerHTML = x;
-}
-
-function myFunction(Winner)
-{
-	var y = document.getElementById('Winner').value;
-	return y
-}
-
 function CompTest(x, amount, FirstPlace)
 {
-	console.log(x);
 	var Check = 0;
 	if(x == FirstPlace){
 		Check = "  Good job!  You Won!  Collect!";
@@ -37,3 +24,13 @@ function CompTest(x, amount, FirstPlace)
 	}
 	return Check;
 }
+
+function results()
+{
+	var FirstPlace = WinningHorse();
+	const myString1 = "The Winning Horse is: ";
+	const myString2 = CompTest(document.getElementById('UserGuess').value, document.getElementById('Amount'), FirstPlace);
+
+	document.getElementById('Race').innerHTML = myString1+FirstPlace+myString2;
+}
+
